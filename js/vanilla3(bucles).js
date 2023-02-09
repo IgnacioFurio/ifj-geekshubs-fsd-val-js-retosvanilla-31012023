df262a47bc2fa7a4db920002506097686d61aa81
Ignacio Furió José <bichoifj@gmail.com>
2023-02-09 09:39:03 +0100

    VAnilla 3, loops until 2 finished

---
+++ js/vanilla3(bucles).js
@@ -0,0 +1,36 @@
+// 1. Escribir un script que pida un valor entero y muestre en pantalla una lista de números desde el 0 al
+// valor tecleado. Los números se separarán por comas.
+
+// let cantidad = parseInt(prompt("¿Hasta que número quieres contar?"))
+
+// let resultado = ""
+
+// for(let i = 0; i <= cantidad; i++){
+
+//     let string = i.toString()
+
+//     resultado = string + "," + " " + resultado
+
+// };
+
+//     console.log(resultado);
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// 2. Hacer un programa que cuente de N en N hasta un número M proporcionado por el usuario. El usuario
+// también ha de proporcionar N
+
+// let M = parseInt(prompt("¿Hasta que número quieres contar?"));
+// let N = parseInt(prompt("¿De cuanto en cuanto quieres contar?"));
+
+// let contador = 0
+// let resultado = ""
+// while(contador <= M){
+
+//     contador += N
+
+//     resultado = resultado + "," + " " + contador
+
+// };
+
+// console.log(resultado);
